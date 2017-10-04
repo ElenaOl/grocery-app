908392d50fc32c059266e9c441661fe43fc685ae
ElenaOl <pravutiner@gmail.com>
2017-10-03 22:54:26 -0700

    first use of API

--- controllers/list.js
+++ controllers/list.js
@@ -3,32 +3,7 @@ var request = require('request');
 var router = express.Router();
 var db = require('../models');
 
-var food = 'cucumber';
-request(
-  'http://api.edamam.com/api/food-database/parser?ingr=' + food + '&app_id=63f7abc8&app_key=2738e46d31b312ca0e39c9dca251c866&page=0',
-  function(error, res, body){
-    var answer = JSON.parse(body);
-    var foodUrl = answer.hints[0].food.uri;
-    request.post(
-      'https://api.edamam.com/api/food-database/nutrients?app_id=63f7abc8&app_key=2738e46d31b312ca0e39c9dca251c866',
-      { json: 
-        {
-          "yield": 1,
-          "ingredients": [
-            {
-              "quantity": 1,
-              "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_unit",
-              "foodURI": foodUrl
-            }
-          ]
-        }
-      }, 
-      function (error, response, body) {
-       // console.log(body);
-      }
-    )
-  }
-);
+
 
 
 // GET - return a page with mylist
@@ -71,15 +46,53 @@ router.get('/addItem', function(req, res) {
 
 //display a specific item
 router.get('/:itemName', function(req, res){
-   db.list.findOne({
-      where: { itemName: req.params.itemName},
-      include: [db.user]
-   }).then(function(item) {
-     if (!item) throw Error();
-     res.render('lists/itemdetails', { item: item });
-   }).catch(function(error) {
-      res.status(400).render('main/404');
-    });
+
+
+  var food = req.params.itemName;
+  request(
+    'http://api.edamam.com/api/food-database/parser?ingr=' + food + '&app_id=63f7abc8&app_key=2738e46d31b312ca0e39c9dca251c866&page=0',
+    function(error, result, body){
+      var answer = JSON.parse(body);
+      var foodUrl = answer.hints[0].food.uri;
+      request.post(
+        'https://api.edamam.com/api/food-database/nutrients?app_id=63f7abc8&app_key=2738e46d31b312ca0e39c9dca251c866',
+        { json: 
+          {
+            "yield": 1,
+            "ingredients": [
+              {
+                "quantity": 1,
+                "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_unit",
+                "foodURI": foodUrl
+              }
+            ]
+          }
+        }, 
+        function (error, response, body) {
+         // console.log(body);
+         res.render('lists/itemdetails', { details: body, item: food });
+        }
+      )
+    }
+  );
+
+
+
+
+
+
+
+
+
+  //  db.list.findOne({
+  //     where: { itemName: req.params.itemName},
+  //     include: [db.user]
+  //  }).then(function(item) {
+  //    if (!item) throw Error();
+  //    res.render('lists/itemdetails', { item: item });
+  //  }).catch(function(error) {
+  //     res.status(400).render('main/404');
+  //   });
 });
 
 //deleting from list
